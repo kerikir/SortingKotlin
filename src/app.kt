491eb8ae286605de71cc816ec1335c6091a2e637
kerikir <vlasov-kerik@mail.ru>
2025-08-11 01:22:01 +0300

    move repeat code into fun

--- src/app.kt
+++ src/app.kt
@@ -1,35 +1,48 @@
+import java.math.BigDecimal
+import java.math.RoundingMode
+import kotlin.math.round
 import kotlin.random.Random
 
 fun main()
 {
+    val ch: Char = '=';
+    val numberRepeat: Int = 120;
+
     // Быстрая сортировка
     println("Быстрая сортировка:");
 
     // Печать исходного массива
-    drawLine('=', 120);
-    println("Исходный массив:")
+    drawLine(ch, numberRepeat);
     var arrayNumbersQuickSort: IntArray = fillArrayRandomNumbers(20, 1, 100);
-    for(i in 0..< arrayNumbersQuickSort.size)
-    {
-        print(arrayNumbersQuickSort[i].toString() + "\t");
-    }
-    println();
+    printArrayInfo("Исходный массив", arrayNumbersQuickSort);
 
     // Выполнение быстрой сортировки
     arrayNumbersQuickSort = quickSort(arrayNumbersQuickSort);
 
     // Печать отсортированного массива
-    println("Отсортированный массив:")
-    for(i in 0..< arrayNumbersQuickSort.size)
+    printArrayInfo("Отсортированный массив", arrayNumbersQuickSort);
+    drawLine(ch, numberRepeat);
+    println("\n\n\n")
+
+
+    // Быстрая сортировка
+    println("Сортировка пузырьком:")
+
+    drawLine(ch, numberRepeat)
+    val arrayDoubleBubbleSort = Array<Double>(15)
     {
-        print(arrayNumbersQuickSort[i].toString() + "\t");
+        BigDecimal(Random.nextDouble(5.0)).setScale(
+            2,
+            RoundingMode.HALF_EVEN
+        ).toDouble()
     }
-    println();
-    drawLine('=', 120);
+    printArrayInfo("Исходный массив", arrayDoubleBubbleSort)
 
+    bubbleSort(arrayDoubleBubbleSort)
 
-//    println("Укажите размер массива:");
-//    val sizeArrayQuickSort: Int? = readLine()?.toInt();
+    printArrayInfo("Отсортированный массив", arrayDoubleBubbleSort)
+    drawLine(ch, numberRepeat)
+    println("\n\n\n")
 }
 
 
@@ -58,5 +71,30 @@ fun drawLine(ch: Char, repeat: Int)
     for (i in 1..repeat)
         print(ch);
 
+    println();
+}
+
+
+/**
+ * Печать информации о массиве
+ */
+fun printArrayInfo(info: String, array: IntArray)
+{
+    println("$info :")
+    for(i in 0..< array.size)
+    {
+        print(array[i].toString() + "\t");
+    }
+    println();
+}
+
+
+/**
+ * Печать информации о массиве
+ */
+fun <T: Number> printArrayInfo(info: String, array: Array<T>)
+{
+    println("$info :")
+    array.forEach({ value -> print(value.toString() + "\t") })
     println();
 }
